b3d32d980ef7288c84adc08b0482f59a157c0a08
Edward Zhang <edward64@gmail.com>
2013-02-20 08:18:39 -0500

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,9 @@
-#include "IWApp.h"
 #include "DirectXApp.h"
 #include "ExperimentalApp.h"
 #include "VisualizationApp.h"
-const int CLOSEST_WINDOW = 1;
-const float NEAR_THRESH = 0.001f;
-const float FAR_THRESH = 0.6f;
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow) {
-	//IWApp app;
-	//StereoApp app;
-	//DirectXApp app(true);
-	//MouseExpApp app;
-	//GestureExpApp app;
-	// CPExp
 	App* app;
 	try{
-		//app = new CPExpApp();
 		//app = new GestureExpApp(true, "test", 2);
         app = new CPExpApp(true, "test", 2);
 		//app = new VisualizationApp();
